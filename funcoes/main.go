5f99b004eda572f4bae51c6be2064825af0036a5
matheuscoutinhoo <matheusfcneto@gmail.com>
2025-04-14 19:11:00 -0300

    new repo

---
+++ funcoes/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+// funcoes podem receber parametros e tambem retorná-los, assim como podem retornar mais de um valor
+func dividir(a, b int) (int, int) {
+	return a / b, a % b
+}
+
+// Higher Order Function - função que retorna outra função
+func somarHof(a int) func(int) int {
+	// a func abaixo é um closure, uma função que captura uma var do escopo acima
+	return func(b int) int {
+		return a + b
+	}
+}
+
+// função variãtica: recebe um numero variavel de parametros
+func somarVariatica(nums ...int) int {
+	var out int
+	for _, n := range nums {
+		out += n
+	}
+	return out
+}
+
+func main() {
+	divisao, resto := dividir(3, 2)
+	fmt.Println(divisao, resto)
+	hofResult := somarHof(3)
+	fmt.Println(hofResult(2))
+	fmt.Println(somarVariatica(1, 2, 3, 4))
+}
